755c7aa28d4528fb22fde22270f201d64abc2872
jvoyatz <jvoyatz@csd.uoc.gr>
2014-04-13 14:43:11 +0300

    fixed bugs in files' creation - system speed up

--- IanniRetrievalSystem/src/IndexingProcess/Indexer.java
+++ IanniRetrievalSystem/src/IndexingProcess/Indexer.java
@@ -30,11 +30,13 @@ import java.util.TreeMap;
  */
 public class Indexer {
 
-	private static final String VOCABULARY = "CollectionIndex\\VocabularyFile.txt";
-	private static final String POSTING = "CollectionIndex\\PostingFile.txt";
+	private static final String VOCABULARY = "VocabularyFile.txt";
+	private static final String POSTING = "PostingFile.txt";
 
-	private static HashMap<String,Document> documents;
-	private static TreeMap<String, Integer> vocabulary;
+	private static File[] inputFiles;
+	private static HashMap<Long, Document> documents;
+	private static TreeMap<String, Word> vocabulary;
+	private static HashMap<Long, Short> maxtfdoc;
 	// temporary
 	static OutputStream fw;
 	static PrintStream ps;
@@ -46,77 +48,92 @@ public class Indexer {
 		// B6. builds PostingFile
 		// It follows the parsing, stemming(B3) and analyzing
 		// of multiple documents (B2)
-		InvertedIndex invertedIndex = buildInvertedIndex();
-
-		SortedSet<Document> index = invertedIndex.getIndex();
-
-
+		long start, stop;
+		start = System.currentTimeMillis();
+	
+		Parser parser = new Parser();
+		parser.readDocuments();
+		documents = parser.getDocsList();
+		vocabulary = parser.getVocabulary();
+		maxtfdoc = parser.getMaxtfdoc();
+		
 		createAllFiles();
+		stop = System.currentTimeMillis();
+		printNumOfDiffWords();
+		long fulltime = stop - start;
+		fulltime = (fulltime / 1000)% 60;
+		System.out.println(fulltime);
 
 	}
 
 	/* Returns the size of keys of the index (sum of diff words) */
 	private static void printNumOfDiffWords(
-			HashMap<Word, LinkedList<PostingListNode>> index) {
-		System.out.println("#Num_of_diff_words: " + index.keySet().size());
+			) {
+		System.out.println("#Num_of_diff_words: " + vocabulary.size());
 	}
 
 
-
 	private static void createAllFiles() throws IOException {
-		
 
 		int nextPos = 0;
 		int prevPos = 0;
 		RandomAccessFile postingFile = new RandomAccessFile(POSTING, "rw");
 		RandomAccessFile vocabularyFile = new RandomAccessFile(VOCABULARY, "rw");
-		
-		//create files
-		createFile(POSTING);
-		createFile(VOCABULARY);
-		
-		
-		for (String word : vocabulary.keySet()) {
-			for  (Entry<String, Document> entry : documents.entrySet()) {
-				Document doc = entry.getValue();
-				HashMap<String,ArrayList<Integer>> docPosList = doc.getPosList(); 
-				if (docPosList.containsKey(word)) {
-					postingFile.write(Long.toString(doc.getDocumentID()).getBytes(Charset.forName("UTF-8")));
-					postingFile.write((" " + word + " ").getBytes(Charset.forName("UTF-8")));
-					ArrayList<Integer> wordList = docPosList.get(word);
-					for (int i = 0; i < wordList.size(); i++) {
-						postingFile.write((" " + Integer.toString(wordList.get(i))).getBytes(Charset.forName("UTF-8")));
-					}
-					postingFile.write((" " + Integer.toString(doc.getDocsLinePos()) + " ").getBytes(Charset.forName("UTF-8")));
-					postingFile.write(Integer.toString(doc.getDocsLineSize()).getBytes(Charset.forName("UTF-8")));
-					postingFile.write(System.getProperty("line.separator").getBytes(Charset.forName("UTF-8")));
-				}	
+
+		for (Entry<String, Word> vocEntry : vocabulary.entrySet()) {
+			String wordStr = vocEntry.getKey();
+			Word word = vocEntry.getValue();
+
+			for (Entry<Long, ArrayList<Integer>> entry : word.getDocPosMap()
+					.entrySet()) {
+				Long docId = entry.getKey();
+				ArrayList<Integer> docPosList = entry.getValue();
+				// System.out.print ("Doc ID  " + entry.getKey() + " ");
+
+				postingFile.write(Long.toString(docId).getBytes(
+						Charset.forName("UTF-8")));
+				postingFile.write((" " + wordStr + " ").getBytes(Charset
+						.forName("UTF-8")));
+				Float termFreq = (float) (word.getWordFreqMap().get(docId)
+						.getTF() / (float) maxtfdoc.get(docId));
+				postingFile.write(Float.toString(termFreq).getBytes(
+						Charset.forName("UTF-8")));
+				postingFile.write((" " + Integer.toString(documents.get(docId)
+						.getDocsLinePos())).getBytes(Charset.forName("UTF-8")));
+				postingFile
+						.write((" " + Integer.toString(documents.get(docId)
+								.getDocsLineSize())).getBytes(Charset
+								.forName("UTF-8")));
+				for (int i = 0; i < docPosList.size(); i++)
+					postingFile
+							.write((" " + Integer.toString(docPosList.get(i)))
+									.getBytes(Charset.forName("UTF-8")));
+				postingFile.write(System.getProperty("line.separator")
+						.getBytes(Charset.forName("UTF-8")));
 			}
+
 			prevPos = nextPos;
-			nextPos = (int) postingFile.length();	
-			
-			System.out.println("Write Length : " + prevPos + "  " + nextPos);
-			vocabularyFile.write((word + " ").getBytes(Charset.forName("UTF-8")));
-			vocabularyFile.write((vocabulary.get(word) + " ").getBytes(Charset.forName("UTF-8")));
-			vocabularyFile.write((" " + Integer.toString(prevPos)).getBytes(Charset.forName("UTF-8")));
-			vocabularyFile.write((" " + Integer.toString(nextPos-prevPos)).getBytes(Charset.forName("UTF-8")));
-			vocabularyFile.write(System.getProperty("line.separator").getBytes(Charset.forName("UTF-8")));
+			nextPos = (int) postingFile.length();
+
+
+
+			vocabularyFile.write((wordStr + " ").getBytes(Charset
+					.forName("UTF-8")));
+			vocabularyFile.write(Integer.toString(word.getDocFreq()).getBytes(
+					Charset.forName("UTF-8")));
+			vocabularyFile.write((" " + Integer.toString(prevPos))
+					.getBytes(Charset.forName("UTF-8")));
+			vocabularyFile.write((" " + Integer.toString(nextPos - prevPos))
+					.getBytes(Charset.forName("UTF-8")));
+			vocabularyFile.write(System.getProperty("line.separator").getBytes(
+					Charset.forName("UTF-8")));
 		}
-		//test reading from file
-		try {
-			System.out.println("\n\n\n******REading:");
-			//readFromFile(POSTING,0 ,line.length());
-			System.out.println(readFromFile(POSTING,141,39));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	/*	vocabulary.clear();
+		documents.clear();
+		maxtfdoc.clear();*/
 		postingFile.close();
 		vocabularyFile.close();
 	}
-
-
 
 	private static void createFolder() {
 		File file = new File("CollectionIndex");
@@ -129,9 +146,6 @@ public class Indexer {
 		}
 	}
 
-
-
-
 	private static String readFromFile(String filePath, int position, int size)
 			throws IOException {
 
@@ -139,41 +153,12 @@ public class Indexer {
 		file.seek(position);
 		byte[] bytes = new byte[size];
 		file.read(bytes);
+
 		System.out.println("File Length : " + file.length());
 		file.close();
-		return new String( bytes);
+
+		return new String(bytes);
 	}
-	
-	
-	/* Builds the inverted index, reads all documents */
-	public static InvertedIndex buildInvertedIndex() throws IOException {
-		Document d;
 
-		/* Input Files */
-		File dir = new File("documentCollection");
-		File[] inputFiles = dir.listFiles();
 
-		InvertedIndex index = new InvertedIndex();
-		index.clear();
-
-		/*
-		 * now lets parse the input file we call the parser that returns a set
-		 * of Document objects
-		 */
-		Parser parser = new Parser(inputFiles);
-		parser.readDocuments();
-		documents = parser.getDocsList();
-		vocabulary = parser.getVocabulary();
-		
-		/* put the document list into the InvertedIndex */
-		index.setDocumentList(documents);
-
-	/*	for (Entry<String, Document> entry : documents.entrySet()) {
-		    System.out.print("key,val: ");
-		    System.out.println(entry.getKey() + "," + entry.getValue().getDocumentID());
-		    index.add(entry.getValue());
-		}*/
-
-		return index;
-	}
 }
--- IanniRetrievalSystem/src/IndexingProcess/Word.java
+++ IanniRetrievalSystem/src/IndexingProcess/Word.java
@@ -1,21 +1,26 @@
 package IndexingProcess;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 /* Models the words of a Document */
 public class Word {
 	private String word;						 				/* value of word */
-	private double docFreq;						 				/* the sum of Documents that we can find this word in */
-	private WordFrequency wordFreq;				 				/* the sum of occurrences of this word */
-	private HashMap<String, LinkedList<Integer>> posList;	 	/* the map of Documents that this word exists in 
-																	and the list of positions in this Document*/
+	private Integer docFreq;						 				/* the sum of Documents that we can find this word in */
+	private short maxTF;
+	private HashMap<Long,WordFrequency> wordFreqMap;				 				/* the sum of occurrences of this word */
+	private ArrayList<Integer> posList;	 						/* the map of Documents that this word exists in and the list of positions in this Document*/
+	private HashMap<Long, ArrayList<Integer>> docPosMap;
+	private HashMap<Long,Short> maxTFDoc;
 	
 	Word (String word) {
 		this.word 		= word;
 		this.docFreq 	= 0;
-		this.wordFreq	= new WordFrequency();
-		this.posList 	= new HashMap<String,LinkedList<Integer>>();
+		this.wordFreqMap = new HashMap<Long,WordFrequency>();
+		this.posList 	= new ArrayList<Integer>();
+		this.docPosMap = new  HashMap<Long,ArrayList<Integer>>();
+		this.maxTFDoc = new  HashMap<Long,Short>();
 	}
 
 	/* returns value of word */
@@ -24,42 +29,83 @@ public class Word {
 	}
 
 	/* returns docFreq of this word */
-	public double getDocFreq() {
+	public Integer getDocFreq() {
 		return this.docFreq;
 	}
 
 	/* increments docFreq of this word */
 	public void incrementDocFreq() {
-		this.docFreq ++;
-	}
-	
-	/* returns WordFreq of this word */
-	public WordFrequency getWordFreq() {
-		return this.wordFreq;
-	}
-	
-	/* increments wordFreq of this word */
-	public void incrementWordFreq() {
-		this.wordFreq.increment();
+		this.docFreq++;
 	}
 
 	/* return list of docs that this word exists in */
-	public HashMap<String, LinkedList<Integer>> getPosList() {
+	public ArrayList<Integer> getPosList() {
 		return this.posList;
 	}
-
+	public void  setPosList(ArrayList<Integer> pos) {
+		 this.posList = pos;
+	}
 	/* add the parameter doc-pos to the map of docs-positions of this word */
-	public void addToPosList(String doc, int pos) {
+	public void addToPosList(Long doc,int pos) {	
+		 if (docPosMap.containsKey(doc)) {
+			   ArrayList<Integer> listPos = docPosMap.get(doc);
+			   listPos.add(pos);
+			   docPosMap.put(doc, listPos);
+			  } else {
+			   /* otherwise, add the word and then create new posting list */
+			   ArrayList<Integer> listPos = new ArrayList<Integer>();
+			   listPos.add(pos);
+			   docPosMap.put(doc, listPos);
+			  }	
+	}
+
+	public HashMap<Long,WordFrequency> getWordFreqMap() {
+		return wordFreqMap;
+	}
+
+	public void addWordFreqMap(Long doc) {
+		Short maxtf=0;
 		
-		if (posList.containsKey(doc)) {
-			LinkedList<Integer> listPos = posList.get(doc);
-			listPos.add(pos);
-			posList.put(doc, listPos);
-		} else {
-			/* otherwise, add the word and then create new posting list */
-			LinkedList<Integer> listPos = new LinkedList<Integer>();
-			listPos.add(pos);
-			posList.put(doc, listPos);
+		if(this.wordFreqMap.containsKey(doc))
+		{
+			this.wordFreqMap.get(doc).increment();
+			maxtf = this.wordFreqMap.get(doc).getTF();
+			this.maxTFDoc.put(doc, maxtf);
 		}
+		else
+		{
+			WordFrequency tf = new WordFrequency();
+			tf.increment();
+			this.wordFreqMap.put(doc, tf);
+			this.maxTFDoc.put(doc, tf.getTF());
+		}
+		
+		
+	}
+
+	public HashMap<Long, ArrayList<Integer>> getDocPosMap() {
+		return docPosMap;
+	}
+
+	public void addDocPosMap(Long doc) {
+		this.docPosMap.put(doc, this.posList);
+	}
+
+	public short getMaxTF(Long id) {
+		return maxTFDoc.get(id);
+	}
+
+	public void setMaxTF(short maxTF) {
+		this.maxTF = maxTF;
+	}
+
+	public HashMap<Long,Short>  getMaxTFDoc() {
+		return maxTFDoc;
+	}
+	public void setMaxTFDoc(HashMap<Long,Short> map) {
+		this.maxTFDoc = map;
+	}	
+	public void MaxTFDoc(long id, short maxTFDoc) {
+		this.maxTFDoc.put(id, maxTFDoc);
 	}	
 }	
